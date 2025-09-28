1ff770aa60e9f12743ae686430c861844846f50b
I4104 Night Owl <I4104.vn@gmail.com>
2025-09-29 02:11:51 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+# INSTALL YAY HELPER
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# INSTALL XCB
+sudo pacman -Syu python-pip libxcb xcb-util xcb-util-wm xcb-util-keysyms
